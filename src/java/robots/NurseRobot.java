27d4b9359ab1fbebaabc260e617f3c86a57ef90e
vtson <sonvt@stringee.com>
2024-07-24 12:58:48 +0700

    feature robots

---
+++ src/java/robots/NurseRobot.java
@@ -0,0 +1,7 @@
+public class NurseRobot implements MedicalRobot {
+    @Override
+    public void care() {
+        // Implementation of medical care
+        System.out.println("Providing medical care...");
+    }
+}
